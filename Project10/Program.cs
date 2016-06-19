200fa81a2d31f3fa75ae3b61317e848d1f5fdc19
Tenukitime <kyeegray@gmail.com>
2016-06-19 01:33:35 -0600

    Project 10

---
+++ Project10/Program.cs
@@ -0,0 +1,75 @@
+﻿// File Prologue
+// Name: Kyle Gray
+// Project: Project 10
+// Date: 6/18/2016
+
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project.  I understand that copying
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in vaolation of 
+// this policy.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Project10
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Bowling bowlers = new Bowling();
+            const int MAX = 10;
+            string[] info = new string[MAX];
+            int length = 0;
+            
+
+           
+            WriteLine("Please enter the bowlers name and their score in the following format \"Mary 135\". You may enter up to 10 players per team. Simply hit enter when finished");
+
+            for (int i = 0; i < MAX; i++)
+            {
+                string temp = ReadLine();
+                if (temp != "")
+                {
+                    info[i] = temp;
+                    length++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+                
+            string[] names = new string[length];
+            int[] scores = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                string[] temporary = info[i].Split();
+                names[i] = temporary[0];
+                scores[i] = int.Parse(temporary[1]);
+                   
+            }
+
+            string[] sortedNames = new string[length];
+            
+
+            bowlers.Sort(scores, names, ref sortedNames, length);
+            foreach(string name in sortedNames)
+            {
+                WriteLine(name);
+            }
+
+            WriteLine($"The average score for the team was {bowlers.CalcAverage(scores):f2}.");
+
+            ReadKey();
+        }
+    }
+}
---
+++ Project10/bowling.cs
@@ -0,0 +1,66 @@
+﻿// File Prologue
+// Name: Kyle Gray
+// Project: Project 10
+// Date: 6/18/2016
+
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project.  I understand that copying
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in vaolation of 
+// this policy.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project10
+{
+    class Bowling
+    {
+
+        // The Sort Method
+        // Purpose:  Sorts scores from highest to lowest, fills an empty array with their scores
+        // Parameters: int[] scores, string[] names, ref string[] sortedNames, int length
+        // Returns: void, but returns sorted names by reference.
+        public void Sort(int[] scores, string[] names, ref string[] sortedNames, int length)
+        {
+            int counter = 0;
+            for (int i = 300; i >= 0; i--)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if ( i == scores[j])
+                    {
+                        sortedNames[counter] = names[j] + "\t" + scores[j];
+                        if(i == 300)
+                        {
+                            sortedNames[counter] += "*";
+                        }                       
+                        counter++;
+
+                    }
+
+                }
+            }
+        }
+
+        // The CalcAverage Method
+        // Purpose: Calculates and returns the average score of the team
+        // Parameters: int[] scores
+        // Returns: double average
+        public double CalcAverage(int[] scores)
+        {
+            int sum = 0;
+            foreach (int score in scores)
+            {
+                sum += score;
+            }
+
+            double average = (double)sum / scores.Length;
+            return average;
+        }
+    }
+}
